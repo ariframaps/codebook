5a45545cbea63b497a390ca05c020988127fed90
ariframaps <ariframa@student.ub.ac.id>
2024-04-16 20:29:46 +0700

    fix responsive open main menu

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -10,8 +10,9 @@ export const Header = () => {
     // search input value
     const [searchVal, setSearchVal] = useState('')
 
-    //dropdown user logo
+    //dropdown
     const [showDropdown, setShowDropdown] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
 
     // navlink active styles
     const classActive = 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500';
@@ -72,14 +73,14 @@ export const Header = () => {
                             </ul >
                         </div >
                         {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
-                        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        <button onClick={() => setShowMenu(!showMenu)} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
                     </div>
-                    <div className="items-center justify-between text-start w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <div className={`${showMenu ? '' : 'hidden'} items-center justify-between text-start w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
                         <ul className="flex flex-col items-stretch md:items-center p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <NavLink to="/" className={({ isActive }) => isActive ? classActive : classInactive} aria-current="page">Home</NavLink>
