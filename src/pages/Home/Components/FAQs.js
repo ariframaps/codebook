783efb91ac7826d1be3fefff14657bc122afb082
ariframaps <ariframa@student.ub.ac.id>
2024-04-16 22:11:06 +0700

    fixing faq darkmode style

--- src/pages/Home/Components/FAQs.js
+++ src/pages/Home/Components/FAQs.js
@@ -1,7 +1,7 @@
 export const FAQs = () => {
     return (
-        <section className="mb-20">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+        <section className="pb-20 dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Question in mind?</h2>
                     <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Frequently asked questions on codebook website</p>
