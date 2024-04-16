e914e85ca68bbe8a4ee4da2e37424ee2bacdbb92
ariframaps <ariframa@student.ub.ac.id>
2024-04-16 20:43:31 +0700

    homepage section featured Products

--- src/pages/Home/Components/FeaturedProdct.js
+++ src/pages/Home/Components/FeaturedProdct.js
@@ -1,5 +1,19 @@
+import { ProductCard } from "../../../components"
+
 export const FeaturedProdct = () => {
     return (
-        <section>FeaturedProdct</section>
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+                <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Featured eBooks</h2>
+                    <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Best selling collection of ebook on codebook this year with good reviews</p>
+                </div>
+                <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-3">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
+            </div>
+        </section>
     )
 }
