44edd27991bbbaf9fb67aa227382ea6d97255cde
ariframaps <ariframa@student.ub.ac.id>
2024-04-18 20:55:09 +0700

    product list api fetch and render

--- src/pages/Products/ProductListPage.js
+++ src/pages/Products/ProductListPage.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react"
+import { ProductCard } from "../../components"
+
 export const ProductListPage = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('http://localhost:8000/products')
+            const result = await response.json()
+            setProducts(result)
+        }
+        fetchProducts()
+    }, [])
+
     return (
-        <main>ProductListPage</main>
+        <main>
+            <div className=" max-w-screen-xl mx-auto p-4">
+                <div className="flex justify-between my-6 text-2xl">
+                    <span>All eBooks(10)</span>
+                    <button>Filter <i class="bi bi-sliders2 p-1"></i></button>
+                </div>
+                <div className="grid grid-cols-3 gap-y-8 justify-items-center my-6">
+                    {products.map(product => (
+                        <ProductCard product={product} />
+                    ))}
+                </div>
+            </div>
+
+        </main>
     )
 }
