79a0ab424f9f8a5591c81061cb7ef53818ecfba1
ariframaps <ariframa@student.ub.ac.id>
2024-04-18 20:07:45 +0700

    added rating loop logic and fixing product card image

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom"
+import { Rating } from "./Rating"
 
-export const ProductCard = ({ key, product }) => {
+export const ProductCard = ({ product }) => {
+
+    // product thumbnail styles
+    const imgStyle = (url) => {
+        const style = {
+            width: '384px',
+            height: '250px',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            background: `url('${url}')`
+        }
+        return style;
+    }
 
     const { id, poster, name, overview, rating, price } = product
 
@@ -9,32 +22,16 @@ export const ProductCard = ({ key, product }) => {
             <span className="absolute top-4 left-4 bg-orange-500 px-3 py-1.5 rounded text-white">Best seller</span>
             <div>
                 <Link to={`/product?id=${id}`}>
-                    <img className="p-8 rounded-t-lg" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product" />
+                    <div style={imgStyle(poster)} className="rounded-t-lg"></div>
                 </Link>
-                <Link to={`/product?id=${id}`} className="px-5 pb-5 block">
+                <Link to={`/product?id=${id}`} className="px-5 mt-3 block">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                 </Link>
             </div>
             <div className="px-5 pb-5">
                 <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">{overview}</p>
                 <div className="flex items-center mt-2.5 mb-5">
-                    <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                    </div>
+                    <Rating rating={rating} />
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{rating}.0</span>
                 </div>
                 <div className="flex items-center justify-between">
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 // element
 import { ProductCard } from "./Elements/ProductCard";
 import { SearchBar } from "./Elements/SearchBar";
+import { Rating } from "./Elements/Rating";
 
 // layout
 import { Header } from "./Layouts/Header";
@@ -9,4 +10,4 @@ import { Footer } from "./Layouts/Footer";
 // other
 import ScrollToTop from "./Other/ScrollToTop";
 
-export { ProductCard, SearchBar, Header, Footer, ScrollToTop }
+export { ProductCard, SearchBar, Header, Footer, ScrollToTop, Rating }
