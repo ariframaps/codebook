e9f7343235b7fa4f0411cddad28dcb93bfb3a102
ariframaps <ariframa@student.ub.ac.id>
2024-04-25 09:40:46 +0700

    fix unused variable

--- src/pages/Cart/Components/Checkout.js
+++ src/pages/Cart/Components/Checkout.js
@@ -29,7 +29,6 @@ export const Checkout = ({ setShowCheckout }) => {
                     user: user
                 }
             })
-            // clearCart();
         } catch (err) {
             navigate('/order-status', {
                 state: {
