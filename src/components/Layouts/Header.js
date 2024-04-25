aa6c2b116664338bd505556b32d3db0fbcf8f77d
ariframaps <ariframa@student.ub.ac.id>
2024-04-25 10:12:56 +0700

    fixes header responsive layout

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -22,7 +22,7 @@ export const Header = ({ showDropdown, setShowDropdown }) => {
     const navigate = useNavigate()
 
     // navlink active styles
-    const classActive = 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500';
+    const classActive = 'block py-1 px-2 md:py-2 md:px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500';
     const classInactive = 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
 
     useEffect(() => {
@@ -39,16 +39,16 @@ export const Header = ({ showDropdown, setShowDropdown }) => {
     return (
         <header>
             <nav className="bg-white dark:bg-gray-900 w-full fixed top-0 z-20 start-0 border-b border-gray-200 dark:border-gray-600" >
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="max-w-screen-xl flex flex-wrap gap-y-3 items-center justify-between mx-auto p-4">
                     <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src={logo} className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeBook</span>
+                        <span className="self-center text-xl md:text-2xl font-semibold whitespace-nowrap dark:text-white">CodeBook</span>
                     </Link>
-                    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <div className="flex md:order-2 items-center space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <div className=" w-full md:block md:w-auto relative" id="navbar-multi-level">
-                            <ul className="flex md:gap-0 gap-2 items-center text-xl font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 space-x-reverse md:flex-row mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <ul className="flex md:gap-0 items-center text-xl font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 space-x-reverse md:flex-row mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 <li>
-                                    <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="block flex items-center p-2 mx-3 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                    <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="block flex items-center p-1 ms-3 mx-1 md:p-2 md:mx-3 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                                         <svg aria-hidden="true" data-toggle-icon={darkMode ? 'sun' : 'moon'} className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             {darkMode ? <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path> : <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>}
                                         </svg>
@@ -56,7 +56,7 @@ export const Header = ({ showDropdown, setShowDropdown }) => {
                                 </li>
                                 <li>
                                     <NavLink to="/cart" className={({ isActive }) => isActive ? classActive : classInactive}>
-                                        <span className="bi bi-bag-fill relative text-2xl">
+                                        <span className="bi bi-bag-fill relative text-xl md:text-2xl">
                                             <span className='absolute left-4 bottom-3 text-sm text-white bg-red-600 px-1 rounded-md'>
                                                 {cartLength}
                                             </span>
@@ -64,7 +64,7 @@ export const Header = ({ showDropdown, setShowDropdown }) => {
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <button onClick={() => setShowDropdown(!showDropdown)} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"><i className="bi bi-person-circle text-2xl"></i>  </button>
+                                    <button onClick={() => setShowDropdown(!showDropdown)} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-1 px-2 md:py-2 md:px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"><i className="bi bi-person-circle text-xl md:text-2xl"></i>  </button>
                                     {showDropdown && (isLoggedIn ? <DropdownLoggedIn /> : <DropdownLoggedOut />)}
                                 </li>
                             </ul >
