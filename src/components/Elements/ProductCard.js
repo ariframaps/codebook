31a232f578f594606f105f76b81a9d59f8ea2390
ariframaps <ariframa@student.ub.ac.id>
2024-06-04 22:00:33 +0700

    change title description and logo

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -22,16 +22,6 @@ export const ProductCard = ({ product }) => {
         red: 'bg-red-700 hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800'
     }
 
-    // product thumbnail styles
-    const imgStyle = (url) => {
-        const style = {
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            background: `url('${url}')`
-        }
-        return style;
-    }
-
     function handleButton() {
         if (!isLoggedIn) {
             navigate('/login');
